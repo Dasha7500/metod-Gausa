c8a3a269f8d6d4befc4561e4fc5eb7609c765b75
daryna.fesenko <darina071602@gmail.com>
2023-07-02 09:44:29 +0200

    Gausa

---
+++ Desktop/метод гауса/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Factorial(n);
+        Console.WriteLine("Произведение чисел от 1 до N: " + result);
+    }
+
+    static int Factorial(int n)
+    {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
